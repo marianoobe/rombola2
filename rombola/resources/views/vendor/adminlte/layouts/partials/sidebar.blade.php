fff1cb20e7b18c9d0a103caa87a69469d2675b4d
fabriciocarrio <fabriciocarrio@gmail.com>
2018-09-07 21:45:57 -0300

    'Cambios en sidebar.blade'

--- rombola/resources/views/vendor/adminlte/layouts/partials/sidebar.blade.php
+++ rombola/resources/views/vendor/adminlte/layouts/partials/sidebar.blade.php
@@ -31,17 +31,26 @@
 
         <!-- Sidebar Menu -->
         <ul class="sidebar-menu">
-            <li class="header">{{ trans('adminlte_lang::message.header') }}</li>
+        <li class="header">Menú</li>
+         <!--<li class="header">{{ trans('adminlte_lang::message.header') }}</li>-->
             <!-- Optionally, you can add icons to the links -->
             <li class="active"><a href="{{ url('home') }}"><i class='fa fa-link'></i> <span>{{ trans('adminlte_lang::message.home') }}</span></a></li>
-            <li><a href="#"><i class='fa fa-link'></i> <span>{{ trans('adminlte_lang::message.anotherlink') }}</span></a></li>
             <li class="treeview">
-                <a href="#"><i class='fa fa-link'></i> <span>{{ trans('adminlte_lang::message.multilevel') }}</span> <i class="fa fa-angle-left pull-right"></i></a>
+                <a href="{{ url('home') }}"><i class='fa fa-user-circle-o '></i> <span>{{ trans('adminlte_lang::message.client') }}</span></a>
                 <ul class="treeview-menu">
-                    <li><a href="#">{{ trans('adminlte_lang::message.linklevel2') }}</a></li>
-                    <li><a href="#">{{ trans('adminlte_lang::message.linklevel2') }}</a></li>
+                    <li><a href="#">{{ trans('adminlte_lang::message.new-client') }}</a></li>
+                    <li><a href="#">{{ trans('adminlte_lang::message.list-client') }}</a></li>
                 </ul>
             </li>
+            <li class="treeview"><a href="{{ url('home') }}"><i class='fa fa-car'></i> <span>{{ trans('adminlte_lang::message.vehicles') }}</span></a>
+                <ul class="treeview-menu">
+                    <li><a href="#">{{ trans('adminlte_lang::message.okm') }}</a></li>
+                    <li><a href="#">{{ trans('adminlte_lang::message.used') }}</a></li>                 
+                </ul>
+            </li>
+            <li class="active"><a href="{{ url('home') }}"><i class='fa fa-link'></i> <span>{{ trans('adminlte_lang::message.presales') }}</span></a></li>
+            <li><a href="#"><i class='fa fa-link'></i> <span>{{ trans('adminlte_lang::message.sales') }}</span></a></li>
+            
         </ul><!-- /.sidebar-menu -->
     </section>
     <!-- /.sidebar -->
