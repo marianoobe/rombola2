565ac5a46274a67f479803091bca286b85db6d8e
Mariano Benitez <benitezmariano5@gmail.com>
2018-11-20 21:15:57 -0300

    usado

--- rombola/resources/views/autos/usados.blade.php
+++ rombola/resources/views/autos/usados.blade.php
@@ -105,7 +105,7 @@
 								<!--Modal -->
 								<div class="modal fade" id="modal-listaNuevo" tabindex="-1" role="dialog" aria-labelledby="modal-listaNuevo"
 								 aria-hidden="true">
-									<div class="modal-dialog" role="document">
+									<div class="modal-dialog" >
 										<div class="modal-content">
 											<div class="modal-header">
 												<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">
